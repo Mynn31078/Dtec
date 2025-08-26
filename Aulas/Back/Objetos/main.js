97ef6993ce759e3f66f9a2e3656cb84913031f1f
Yasmin Brito <yasminbritosilva.31078@gmail.com>
2025-08-26 16:34:57 -0300

    Objetos

---
+++ Aulas/Back/Objetos/main.js
@@ -0,0 +1,76 @@
+const listaDeCompras = {
+    "nome": "Lista de Compras",
+    "data": "21/08/2025",
+    "itens": [
+        {
+            "item": "maçã",
+            "quantidade": 5
+        },
+        {
+            "item": "pão",
+            "quantidade": 10
+        }
+    ]
+}
+
+console.log(listaDeCompras.data)
+console.log(listaDeCompras.nome)
+console.log(listaDeCompras.itens[0].item)
+
+//Construa um objeto chamado perfilUsuário
+//Propriedades devem ser , nome, data de nascimento, email, hobbies
+//a propriedade hobbies deve 
+//ter 5 objetos cada um com um hobbie e um dia da semana
+
+
+const perfilUsuário = {
+    "nome": "Manuella",
+    "data de nascimento": "31/07/2008",
+    "email": "manuella.oliveira.tec@despertar.org.br",
+    "hobbies": [
+        {
+            "hobbie": "xadrez",
+            "dia": "Segunda"
+        },
+        {
+            "hobbie": "videogame",
+            "dia": "Terça"
+        },
+        {
+            "hobbie": "desenhar",
+            "dia": "Quarta"
+        },
+        {
+            "hobbie": "tocar piano",
+            "dia": "Quinta"
+        },
+        {
+            "hobbie": "cantar",
+            "dia": "Sexta"
+        }
+    ]
+}
+
+const catalogo = {
+    "livros": [
+        {
+            "titulo": "Dom Casmurro",
+            "autor": "Machado de Assis",
+            "ano": 1899,
+            "disponivel": false
+        },
+        {
+            "titulo": "A Revolução dos Bichos",
+            "autor": "George Orwell",
+            "ano": 1945,
+            "disponivel": true
+        },
+        {
+            "titulo": "Pequeno Princípe",
+            "autor": "Antoine de Saint-Exupéry",
+            "ano": 1943,
+            "disponivel": true
+        }
+    ]
+}
+
