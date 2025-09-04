8980c82dba133c6e553fd9fa6ef74c09ec3c1342
Yasmin Brito <yasminbritosilva.31078@gmail.com>
2025-09-04 16:23:10 -0300

    Buscar Informações de Países (API)

--- Projetos/Países/script.js
+++ Projetos/Países/script.js
@@ -3,6 +3,13 @@ const paisInput = document.getElementById("paisInput")
 const buscarBtn = document.getElementById("buscarBtn")
 const container = document.getElementById("container")
 
+document.addEventListener('keydown', (event) => {
+    if(event.key === "Enter"){
+        event.preventDefault()
+        buscarBtn.click()
+    }
+})
+
 //Adiciona um "ouvinte" ao evento clique do botão
 buscarBtn.addEventListener('click', () => {
     const nomePais = paisInput.value.trim()
@@ -11,20 +18,33 @@ buscarBtn.addEventListener('click', () => {
         alert("Digite o nome de um país")
         return;
     }
-    const url = `https://restcountries.com/v3.1/name/${nomePais}`
+    const url = `https://restcountries.com/v3.1/translation/${nomePais}`
 
     fetch(url)
         .then(response => {
+
+            if (!response.ok) {
+                throw new Error('País não encontrado')
+            }
+
             return response.json();
         })
         .then(data => {
             const pais = data[0];
 
+            const moeda = Object.values(pais.currencies)[0].name;
+
             container.innerHTML = `
-                <h2>${pais.name.common}</h2>
-            
+                <h2>${pais.translations.por.common}</h2>
+                <img src="${pais.flags.svg}" width="150">
+                <p><strong>Capital:</strong> ${pais.capital[0]}</p>
+                <p><strong>População:</strong> ${pais.population.toLocaleString()}</p>
+                <p><strong>Moeda:</strong>${moeda}</p>
             `
         })
 
-
+        .catch(error => {
+            console.error(error)
+            container.innerHTML = `<p style = "color: red;">Erro: ${error.message}</p>`
+        })
 })
